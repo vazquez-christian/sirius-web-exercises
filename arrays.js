5e33efc0fc5f9f35406b09e7bb7cbb7dddf719ec
Christian Vazquez <vazquez.a.christian36@gmail.com>
2021-12-03 10:04:33 -0700

    arrays practice update, computer crashed

--- arrays.js
+++ arrays.js
@@ -1,8 +1,21 @@
 let students = ["Gerald", "Mark", "Mina", "Jason", "Justin", "Justin"];
 //[] = arrays
 //anything within the commas = elements
-//array indexes always start at 0
+//array indices always start at 0
+//access Justin's value
+// re-assign that value to = justin + lastName
 console.log(students.length)
-console.log(students[3])
-
+console.log(students)
+console.log(students[4])
 let favFoods = ["Pizza", "Tacos", "Burritos", "Eggs", "Chorizo"];
+
+console.log(favFoods)
+favFoods[0] += " 1"
+console.log(favFoods[0])
+favFoods[1] += " 2"
+console.log(favFoods[1])
+favFoods[2] += " 3"
+console.log(favFoods[2])
+
+console.log(students.indexOf("Mina"))
+
