668b19e0b4134a9cd1fd8029876f542ec2d4e0fb
Christian Vazquez <vazquez.a.christian36@gmail.com>
2021-12-03 09:47:38 -0700

    arrays practice

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let students = ["Gerald", "Mark", "Mina", "Jason", "Justin", "Justin"];
+//[] = arrays
+//anything within the commas = elements
+//array indexes always start at 0
+console.log(students.length)
+console.log(students[3])
+
+let favFoods = ["Pizza", "Tacos", "Burritos", "Eggs", "Chorizo"];
