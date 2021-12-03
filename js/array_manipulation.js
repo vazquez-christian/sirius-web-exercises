9598faeebabf0e9ed7054863bf7d22264dcb811b
Christian Vazquez <vazquez.a.christian36@gmail.com>
2021-12-03 15:08:35 -0700

    update with spread operator

--- js/array_manipulation.js
+++ js/array_manipulation.js
@@ -16,3 +16,33 @@ console.log("Removing last index");
 samsDogs.pop()
 
 console.log("after pop", samsDogs);
+
+console.log("Fetching index of \"Duke\"");
+
+console.log(samsDogs.indexOf("Duke"));
+
+let sliced = samsDogs.slice(0, 1);
+
+console.log("sliced", sliced);
+
+console.log("Reversing array...");
+
+samsDogs.reverse();
+
+console.log(samsDogs);
+
+console.log("Sorting array...");
+
+samsDogs.sort();
+
+console.log(samsDogs);
+
+console.log('samsDogs as a string', samsDogs.join(", "));
+
+let myNums = [3, 4, 6, 1]
+
+console.log([...samsDogs, ...myNums]);
+
+console.log([5.3, ...myNums]);
+
+console.log([...myNums, 5.3]);
