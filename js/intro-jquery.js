0d2597e8d8334f105182a4d69c291209f1cc9354
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-01-20 14:26:01 -0600

    jQuery event mouse events exercise done

--- js/intro-jquery.js
+++ js/intro-jquery.js
@@ -14,15 +14,34 @@ $(document).ready(function() {
 //
 //
 
-     // $(".codeup").css("border", "1px solid red")
+    // $(".codeup").css("border", "1px solid red")
 
-    $("li").css("font-size", "20px");
+    // $("li").css("font-size", "20px");
+    //
+    // $("h1, p, li").css("background-color", "yellow");
+    //
+    // let contents = $("h1").html();
+    // alert(contents)
 
-    $("h1, p, li").css("background-color", "yellow");
+    ///////////////////////////////////////////
+    //MOUSE EVENTS//
 
-    let contents = $("h1").html();
-    alert(contents)
+    $("h1").click(function(){
+        $(this).css("background-color", "orange");
+    })
 
+    $("p").dblclick(function (e){
+        $(this).css("font-size", "18px");
+    })
+
+    $("li").hover(
+        function() {
+            $(this).css("color", "red");
+        },
+        function () {
+            $(this).css("color", "black");
+        }
+    )
 
 });
 
