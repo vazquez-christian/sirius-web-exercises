e594544a072f78bb956004d39002c7386f833a4c
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-01-20 11:31:13 -0600

    Jquery intro and selectors exercises

---
+++ js/intro-jquery.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(document).ready(function() {
+//     window.onload = function() {
+//         alert("The page has finished loading!")
+//     }
+//
+//     // let contents = $("#main").html();
+//     // alert(contents);
+//
+//     let contents = $("#main-list").html();
+//     alert(contents);
+// //    when the same ID is set for two elements JQuery selects the first one to appear in the document
+//
+//
+
+     // $(".codeup").css("border", "1px solid red")
+
+    $("li").css("font-size", "20px");
+
+    $("h1, p, li").css("background-color", "yellow");
+
+    let contents = $("h1").html();
+    alert(contents)
+
+
+});
+
