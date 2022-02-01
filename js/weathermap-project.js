6b6b2a9cd3b78fa5a3dca23399f70ed6b3538995
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-01 12:16:29 -0600

    updating weather map project

--- js/weathermap-project.js
+++ js/weathermap-project.js
@@ -1,19 +1,58 @@
+// mapboxgl.accessToken = mapboxKey;
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     zoom: 10,
+//     center: [-98.58803, 29.57544]
+// });
+
+function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = month + ' ' + date + ' ' + year;
+    return time;
+}
+
+let latitude = 29.423017;
+let longitude = -98.48527;
+
+
 mapboxgl.accessToken = mapboxKey;
-var map = new mapboxgl.Map({
+const coordinates = document.getElementById('coordinates');
+const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 10,
-    center: [-98.58803, 29.57544]
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude, latitude],
+    zoom: 9
 });
 
+const marker = new mapboxgl.Marker({
+    draggable: true
+})
+    .setLngLat([longitude, latitude])
+    .addTo(map);
 
-$.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${29.423017}&lon=${-98.48527}&exclude=minutely,hourly&appid=${weatherMapKey}&units=imperial`).done(function (data) {
-    console.log(data);
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+}
+
+marker.on('dragend', onDragEnd);
+
+$.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${weatherMapKey}&units=imperial`).done(function (data) {
+    // console.log(data);
     for (let i = 0; i < 5; i++) {
-        $("#weather").append(`<div>${data.daily[i].temp.day}° / ${data.daily[i].temp.night}</div>`,
-            `<div>Description: ${data.daily[i].weather[0].description}</div>`,
-            `<div>Humidity: ${data.daily[i].humidity}</div>`,
-            `<div>Wind Speed: ${data.daily[i].wind_speed}</div>`)
+        console.log(data)
+        $("#weather").append(`<div class="col card">
+            <div class="">${data.daily[i].temp.day}° / ${data.daily[i].temp.night}</div>
+            <div class="">Description: ${data.daily[i].weather[0].description}</div>
+            <div class="">Humidity: ${data.daily[i].humidity}</div>
+            <div class="">Wind Speed: ${data.daily[i].wind_speed}</div>
+            </div>`)
     }
     // $(".container").append(`<p>${data.daily[0].temp.day}°</p>`)
 })
@@ -26,11 +65,5 @@ $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${29.423017}&lon=${-9
 //     center: [-98.58803, 29.57544]
 // });
 
-$("#mapPlace").click(function () {
-    $("#place")
-        new MapboxGeocoder({
-            accessToken: mapboxKey,
-            mapboxgl: mapboxgl
-        })
 
-})
+
